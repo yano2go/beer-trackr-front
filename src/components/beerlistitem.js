3736132fcc857432eb063574e0aa678ef12d08f7
Alex Yanovich <alexyanovich@Alexs-MacBook-Pro.local>
2020-09-24 21:50:47 -0400

    making progress

--- src/components/beerlistitem.js
+++ src/components/beerlistitem.js
@@ -4,7 +4,7 @@ export default class beerlistitem extends Component {
      render() {
           return (
                <div>
-                   `${JSON.stringify(beerData)}`
+                   `${JSON.stringify(formInputs)}`
                </div>
           )
      }
